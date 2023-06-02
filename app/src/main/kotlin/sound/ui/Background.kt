4a66b59a12356fffd5d0022c6db2afeb8e3ea539
chelate1118 <gs21077@gs.hs.kr>
2023-06-02 16:15:56 +0900

    add background component

---
+++ app/src/main/kotlin/sound/ui/Background.kt
@@ -0,0 +1,23 @@
+package sound.ui
+
+import processing.core.PVector
+import sound.Processing
+import sound.ui.filter.FilterPoint
+
+class Background(sketch: Processing): Component(sketch, Order.Priority.Background) {
+    val color = sketch.color(152, 238, 204)
+    
+    override fun update(): State = State.Alive
+
+    override fun display() {
+        sketch.background(color)
+    }
+    
+    override fun isMouseIn(position: PVector): Boolean = true
+
+    override fun mouseClicked() {
+        with (sketch) {
+            ui.createComponent(FilterPoint(this, mousePosition))
+        }
+    }
+}
