91439a006be7a170b54ecb519175e0d1d6bf6018
chelate1118 <gs21077@gs.hs.kr>
2023-06-05 15:30:22 +0900

    add line method

--- app/src/main/kotlin/sound/Advanced.kt
+++ app/src/main/kotlin/sound/Advanced.kt
@@ -5,4 +5,8 @@ import processing.core.*
 open class AdvancedPApplet: PApplet() {
     val mousePosition
         get() = PVector(mouseX.toFloat(), mouseY.toFloat())
+
+    fun line(vec1: PVector, vec2: PVector) {
+        line(vec1.x, vec1.y, vec2.x, vec2.y)
+    }
 }
