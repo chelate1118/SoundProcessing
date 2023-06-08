082f3cef4a3de430441b3df07c928d3c3507ad80
chelate1118 <gs21077@gs.hs.kr>
2023-06-08 12:38:53 +0900

    add sleep

--- RustCore/src/main.rs
+++ RustCore/src/main.rs
@@ -7,7 +7,8 @@ extern crate lazy_static;
 #[tokio::main]
 async fn main() {
     println!("Start");
-    sound::audio_play();
+    std::thread::spawn(sound::audio_play);
+    spin_sleep::sleep(std::time::Duration::from_secs(7));
 
 //    kotlin::start_connection().await.unwrap();
 }
