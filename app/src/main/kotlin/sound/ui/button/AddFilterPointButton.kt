35240a643ce7687757398a54e2c1637ccaf96e28
chelate1118 <gs21077@gs.hs.kr>
2023-06-19 15:01:17 +0900

    Create PImage objects for add and file open button images
    Added PImage objects for the 'add' and 'file open' button images in their respective class files to improve display and functionality.

--- app/src/main/kotlin/sound/ui/button/AddFilterPointButton.kt
+++ app/src/main/kotlin/sound/ui/button/AddFilterPointButton.kt
@@ -1,14 +1,28 @@
 package sound.ui.button
 
+import processing.core.PImage
 import processing.core.PVector
 import sound.Processing
 import sound.ui.filter.FilterPoint
 
 class AddFilterPointButton(sketch: Processing): Button(sketch) {
+    companion object {
+        private const val IMAGE_WIDTH = 25F
+        private const val IMAGE_HEIGHT = 25F
+    }
+
     override val position = PVector(sketch.width - buttonWidth, buttonHeight)
-    
+    private val addImage: PImage = sketch.loadImage("add.png")
+
     override fun display() {
         super.display()
+
+        with (sketch) {
+            pushMatrix()
+            translate(position.x, position.y)
+            image(addImage, -IMAGE_WIDTH/2F, -IMAGE_HEIGHT/2F, IMAGE_WIDTH, IMAGE_HEIGHT)
+            popMatrix()
+        }
     }
     
     override fun mouseClicked() {
--- app/src/main/kotlin/sound/ui/button/LoadFileButton.kt
+++ app/src/main/kotlin/sound/ui/button/LoadFileButton.kt
@@ -1,13 +1,27 @@
 package sound.ui.button
 
+import processing.core.PImage
 import processing.core.PVector
 import sound.Processing
 
 class LoadFileButton(sketch: Processing): Button(sketch) {
+    companion object {
+        private const val IMAGE_WIDTH = 25F
+        private const val IMAGE_HEIGHT = 25F
+    }
+
     override val position = PVector(sketch.width - buttonWidth * 2.5F, buttonHeight)
+    private val loadFileImage: PImage = sketch.loadImage("file_open.png")
 
     override fun display() {
         super.display()
+
+        with (sketch) {
+            pushMatrix()
+            translate(position.x, position.y)
+            image(loadFileImage, -IMAGE_WIDTH/2F, -IMAGE_HEIGHT/2F, IMAGE_WIDTH, IMAGE_HEIGHT)
+            popMatrix()
+        }
     }
 
     override fun mouseClicked() {
